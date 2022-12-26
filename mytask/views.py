8fc53dd3467a02124484efd9e9cb640a084275d0
Srijan Srivastava <ss02@192.168.1.29>
2022-12-26 12:59:27 +0530

    Model form

--- mytask/views.py
+++ mytask/views.py
@@ -3,12 +3,9 @@ from django.http import HttpResponseRedirect
 from django import forms
 from django.urls import reverse
 
+
 tasks= []   #creating empty list to store task
 # Create your views here.
-
-
-class NewtaskForm(forms.Form):
-    task=forms.CharField(label="Add_Task")
 
 
 
@@ -24,7 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
-            return HttpResponseRedirect(reverse())
+            return HttpResponseRedirect(reverse("index"))
     return render(request, "mytask/add.html", {
         "form": NewtaskForm()
     })    
